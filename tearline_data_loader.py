7c9df464d674e876ee1afb984e5ab7f2a1af5123
rthenhaus <rthenhaus>
2023-12-13 12:07:01 -0600

    Added cowc data loader and training driver, not tested yet

--- tearline_data_loader.py
+++ tearline_data_loader.py
@@ -1,6 +1,8 @@
-import os 
+import os
+import pickle 
 from PIL import Image 
 from torch.utils import data
+from tqdm import tqdm
 
 # Currently trying to create data loader for COWC images
 
@@ -13,6 +15,42 @@ class Cowc(data.Dataset):
                  label_file=None,
                  num_classes=1,
                  split="train",
-                 transform=None)
-    assert split in ["train", "val", "test"]
+                 transform=None):
+        assert split in ["train", "val", "test"]
+        # root folder, split 
+        self.root_folder = os.path.join(root, "64x64_15cm_24px-exc_v5-marg-32_expanded")
+        self.split = split 
+        self.transform = transform 
+        self.n_classes = num_classes 
+
+        # load labels 
+        if label_file is None:
+            label_file = os.path.join(self.root_folder, "toronto_test_label.txt")
+        if not os.path.exists(label_file):
+            raise ValueError(
+                "Label file {:s} does not exist!".format(label_file))
+        with open(label_file) as f:
+            lines = f.readlines()
+        
+        # store the file list 
+        file_label_list = []
+        for line in lines:
+            tmp = line.rstrip('\n').split(' ')
+            filename = tmp[0]
+            label_id = int(tmp[-1])
+            file_label_list.append((filename, label_id))
+        
+        # load 
+        self.img_label_list = self._load_dataset(file_label_list)
+
+    def _load_dataset(self, file_label_list):
+        # load dataset into memory
+        print("Loading {:s} set into memroy. This might take a while...".format(self.split))
+        img_label_list = tuple()
+        for filename, label_id in tqdm(file_label_list):
+            img = Image.open(filename).convert('RGB')
+            # Images are currently 64x64 
+            label = label_id
+            img_label_list += ((img, label), )
+        return img_label_list
 
---
+++ train_tearline.py
@@ -0,0 +1,106 @@
+import os
+import argparse 
+import torch 
+import torch.nn as nn 
+import torch.optim as optim 
+import torchvision.transforms as transforms
+from tearline_data_loader import Cowc
+from student_code import LeNet, train_model, test_model
+
+def save_checkpoint(state, is_best, file_folder="./outputs/",
+                    filename="checkpoint.pth.tar"):
+    """save checkpoint"""
+    if not os.path.exists(file_folder):
+        os.makedirs(os.path.expanduser(file_folder), exist_ok=True)
+    torch.save(state, os.path.join(file_folder, filename))
+    if is_best:
+        # skip optimization state 
+        state.pop("optimizer", None)
+        torch.save(state, os.path.join(file_folder, "model_best.pth.tar"))
+
+
+# main function for training and testing
+def main(args):
+    # set up random seed
+    torch.manual_seed(0)
+
+    ###################################
+    # setup model, loss and optimizer #
+    ###################################
+    model = LeNet()
+
+    training_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+
+    # set up transforms to transform the PIL Image to tensors
+    train_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    ################################
+    # setup dataset and dataloader #
+    ################################
+    train_set = Cowc(root="", split="train", transform=train_transforms)
+    test_set = Cowc(root="", split="val", transform=test_transform)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=args.batch_size, shuffle=False)
+
+    ##################
+    # start training #
+    ##################
+
+    # resume from a previous checkpoint 
+    best_acc = 0.0 
+    start_epoch = 0 
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{:s}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            start_epoch = checkpoint['epoch']
+            best_acc = checkpoint['best_acc']
+            # load model weight
+            model.load_state_dict(checkpoint['state_dict'])
+            # load optimizer states
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{:s}' (epoch {:d}, acc {:0.2f})".format(args.resume, start_epoch, 100*best_acc))
+    else:
+        print("=> no checkpoint found at '{}'".format(args.resume))
+        return 
+    
+    # train 
+    print("Training the model...\n")
+    for epoch in range(start_epoch, args.epochs):
+            # train model for 1 epoch 
+            train_model(model, train_loader, optimizer, training_criterion, epoch)
+        # evaluate the model on test_set after this epoch
+        acc = test_model(model, test_loader, epoch)
+        # save the current checkpoint 
+        save_checkpoint({
+            "epoch": epoch + 1,
+            "state_dict": model.state_dict(),
+            "best_acc": max(best_acc, acc),
+            "optimizer": optimizer.state_dict(),
+            }, (acc > best_acc))
+        best_acc = max(best_acc, acc)
+    print("Finished Training")
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Image Classification (cars) using Pytorch")
+    parser.add_argument("--epochs", default=10, type=int, metavar="N", help="number of total epochs to run")
+    parser.add_argument("--lr", default=0.001, type=float, metavar="LR", help="initial learning rate", dest="lr")
+    parser.add_argument("--batch_size", default=32, type=int, metavar="N", help="number of images within a mini-batch")
+    parser.add_argument("--resume", default="", type=str, metavar="PATH", help="path to latest checkpoint (default: none)")
+    args = parser.parse_args()
+    main(args)
