85f5cb550bbd58bf6fc3c203c32f1b7725d6467b
rthenhaus <rthenhaus>
2023-12-13 21:14:37 -0600

    Added run_model.py model doesn't seem to be as accurate on Hamhung data, need to format data better

--- lenet.py
+++ lenet.py
@@ -111,14 +111,17 @@ def test_model(model, test_loader, epoch):
     model.eval()
     correct = 0
     with torch.no_grad():
-        for input, target in test_loader:
+         for input, target in test_loader:
             output, _ = model(input)
             # pred = output.max(1, keepdim=True)[1]
             _, pred = torch.max(output.data, 1)
+            classes = ["not car", "other", "pickup", "sedan"]
+            # print("Predicted: " + classes[pred[4]])
             # print("pred: " + str(pred) + " target: " + str(target))
             # print(pred.eq(target.view_as(pred)).sum().item())
             # pred.eq
             correct += pred.eq(target.view_as(pred)).sum().item()
+            # exit()
 
     test_acc = correct / len(test_loader.dataset)
     print('[Test set] Epoch: {:d}, Accuracy: {:.2f}%\n'.format(
---
+++ run_model.py
@@ -0,0 +1,106 @@
+import os
+import time 
+import argparse 
+
+import torch
+from torch.utils.checkpoint import checkpoint 
+import torchvision.transforms as transforms
+from tearline_data_loader import Cowc
+from lenet import LeNet, test_model
+from tqdm import tqdm
+from PIL import Image
+
+
+class Target():
+    """Dataloader for target data"""
+    def __init__(self,
+                 path,
+                 transform=None):
+        self.root_folder = os.path.join("./", path)
+        self.transform = transform
+        self.path = path
+        # load data
+        if not args.path:
+            print("Please specify a path to images with --path")
+            return
+
+        print("Loading {:s} into memory...".format(args.path))
+        files = os.listdir(args.path)
+        image_list = tuple() 
+        for f in tqdm(files):
+            img = Image.open(os.path.join(self.root_folder, f)).convert("L")
+            img = img.resize((32, 32), Image.BILINEAR)
+            image_list += ((img, -1), )
+        
+        self.image_list = image_list
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, index):
+        # load img and label 
+        img, label = self.image_list[index]
+        
+        # apply data augmentation
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, label
+
+
+def main(args):
+    # set up random seed 
+    torch.manual_seed(0)
+
+    model = LeNet()
+    
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=0.456, std=0.224)
+    ])
+    target = Target(args.path, transform=test_transform)
+    data_loader = torch.utils.data.DataLoader(
+        target, batch_size=32, shuffle=False)
+
+    # run model 
+    if not args.load:
+        args.load = "./outputs/model_best.pth.tar"
+    if os.path.isfile(args.load):
+        print("=> loading model '{:s}'".format(args.load))
+        checkpoint = torch.load(args.load)
+        # load model weight
+        model.load_state_dict(checkpoint['state_dict'])
+        epoch = checkpoint['epoch']
+        print("=> loaded model '{:s}' (epoch {:d})".format(args.load, checkpoint['epoch']))
+    else:
+        print("=> no model found at '{}'".format(args.load))
+        return
+
+    # running and timing
+    print("Running the model...\n")
+    start = time.time()
+    model.eval()
+    cars = 0 
+    with torch.no_grad():
+        for input, _ in data_loader:
+            output, _ = model(input)
+            pred = output.max(1, keepdim=True)[1] 
+            classes = ["not car", "other", "pickup", "sedan"]
+            for p in pred:
+                if classes[p] == "sedan": cars += 1
+        print("cars: " + str(cars))
+
+
+
+    end = time.time() 
+    print("Model took {:0.2f} sec".format(end-start))
+
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Image Classification (cars) using Pytorch")
+    parser.add_argument("--path", default="", type=str, metavar="PATH",
+                        help="path to directory of 64x64 images to run model on")
+    parser.add_argument("--load", default="", type=str, metavar="PATH",
+                        help="path to saved model")
+    args = parser.parse_args()
+    main(args)
